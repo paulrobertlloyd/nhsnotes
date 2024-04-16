68cd931dc0234c9fbd1f3fb071ba1d0cce0f0e1e
Frankie Roberto <frankie@frankieroberto.com>
2024-04-16 12:19:02 +0100

    Initial post

---
+++ eleventy.config.js
@@ -0,0 +1,34 @@
+const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+
+module.exports = function(eleventyConfig) {
+  // Register the plugin
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    header: {
+      logotype: {
+        text: 'NHSnotes'
+      },
+      productName: ""
+    },
+    footer: {
+      copyright: {
+        text: '© 2024'
+      }
+    }
+  })
+
+  // Collections
+  eleventyConfig.addCollection('post', (collection) => {
+    return collection.getFilteredByGlob('posts/*.md')
+  })
+
+
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      // Use layouts from the plugin
+      layouts: 'node_modules/@x-govuk/govuk-eleventy-plugin/layouts'
+    }
+  }
+};
---
+++ index.md
@@ -0,0 +1,11 @@
+---
+homepage: true
+layout: collection
+title: Notes on working on NHS digital prevention services
+description: From Frankie Roberto
+pagination:
+  data: collections.post
+  size: 20
+  reverse: true
+---
+
